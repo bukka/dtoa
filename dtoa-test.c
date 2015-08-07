4603979b949440e8c5d78f1c140375a5d8b89077
Jakub Zelenka <bukka@php.net>
2015-08-07 20:51:56 +0100

    Fix dtoa-test and add some more examples

--- dtoa-test.c
+++ dtoa-test.c
@@ -12,29 +12,33 @@
 
 #define DEBUG 1
 
+#define Long int
+
 #include "dtoa.c"
 
 void echo_dval(double dd, int mode, int ndigits)
 {
 	int sign, decpt;
-	char *rve;
-	char *s = dtoa(dd, mode, ndigits, &sign, &decpt, NULL);
+	char *s = dtoa(dd, mode, ndigits, &decpt, &sign, NULL);
 	
-	printf("%-25s: sign: %d, decpt: %d\n", s, sign, decpt);
+	printf("%-80s -- sign: %d, decpt: %d\n", s, sign, decpt);
 	freedtoa(s);
 }
 
 void echo_dvals(const char *title, double dd)
 {
-	printf("\ntitle\n");
-	echo_dval(dd, 0, 14);
-	echo_dval(dd, 0, 17);
-	echo_dval(dd, 2, 14);
+	printf("\n%s\n", title);
+	echo_dval(dd, 0, 0);
 	echo_dval(dd, 2, 17);
+	echo_dval(dd, 2, 100);
 }
 
 int main()
 {
-	echo_dvals("0.1234567890123456789 in C", 0.123);
+	echo_dvals("0.5 in C", 0.5);
+	echo_dvals("0.00001234567890123456789 in C", 0.00001234567890123456789);
+	echo_dvals("-12323.23422134233 in C", -12323.23422134233);
+	echo_dvals("123232394239048932930489349234903422134233", 123232394239048932930489349234903422134233.1);
 	echo_dvals("6 / 3", 6.0 / 3.0);
+	echo_dvals("0.908234 / 0.293023323234", 0.908234 / 0.293023323234);
 }
