2d5e3c8b31ddb0b0566a1229170b25c8140a51c2
Jakub Zelenka <bukka@php.net>
2015-08-16 20:20:26 +0100

    Add gcvt output

--- dtoa-test.c
+++ dtoa-test.c
@@ -16,18 +16,118 @@
 
 #include "dtoa.c"
 
+
+char *echo_gcvt(int ndigit, char dec_point, char exponent, char *buf,
+				char *digits, int sign, int decpt)
+{
+	char *dst, *src;
+	int i;
+
+	if (decpt == 9999) {
+		/*
+		 * Infinity or NaN, convert to inf or nan with sign.
+		 * We assume the buffer is at least ndigit long.
+		 */
+		i = 0;
+		if (sign && *digits == 'I') {
+			buf[i++] = '-';
+		}
+		strncpy(&buf[i], *digits == 'I' ? "INF" : "NAN", 4);
+		return (buf);
+	}
+
+	dst = buf;
+	if (sign) {
+		*dst++ = '-';
+	}
+
+	if ((decpt >= 0 && decpt > ndigit) || decpt < -3) { /* use E-style */
+		/* exponential format (e.g. 1.2345e+13) */
+		if (--decpt < 0) {
+			sign = 1;
+			decpt = -decpt;
+		} else {
+			sign = 0;
+		}
+		src = digits;
+		*dst++ = *src++;
+		*dst++ = dec_point;
+		if (*src == '\0') {
+			*dst++ = '0';
+		} else {
+			do {
+				*dst++ = *src++;
+			} while (*src != '\0');
+		}
+		*dst++ = exponent;
+		if (sign) {
+			*dst++ = '-';
+		} else {
+			*dst++ = '+';
+		}
+		if (decpt < 10) {
+			*dst++ = '0' + decpt;
+			*dst = '\0';
+		} else {
+			/* XXX - optimize */
+			for (sign = decpt, i = 0; (sign /= 10) != 0; i++)
+				continue;
+			dst[i + 1] = '\0';
+			while (decpt != 0) {
+				dst[i--] = '0' + decpt % 10;
+				decpt /= 10;
+			}
+		}
+	} else if (decpt < 0) {
+		/* standard format 0. */
+		*dst++ = '0';   /* zero before decimal point */
+		*dst++ = dec_point;
+		do {
+			*dst++ = '0';
+		} while (++decpt < 0);
+		src = digits;
+		while (*src != '\0') {
+			*dst++ = *src++;
+		}
+		*dst = '\0';
+	} else {
+		/* standard format */
+		for (i = 0, src = digits; i < decpt; i++) {
+			if (*src != '\0') {
+				*dst++ = *src++;
+			} else {
+				*dst++ = '0';
+			}
+		}
+		if (*src != '\0') {
+			if (src == digits) {
+				*dst++ = '0';   /* zero before decimal point */
+			}
+			*dst++ = dec_point;
+			for (i = decpt; digits[i] != '\0'; i++) {
+                *dst++ = digits[i];
+            }
+        }
+        *dst = '\0';
+    }
+    return (buf);
+}
+
 void echo_dval(double dd, int mode, int ndigits)
 {
 	int sign, decpt;
+	char buf[2048];
 	char *s = dtoa(dd, mode, ndigits, &decpt, &sign, NULL);
 	
-	printf(" %d | %2d | %-55s | %4d | %5d\n", mode, ndigits, s, sign, decpt);
+	printf(" %d | %2d | %-55s | %4d | %5d | %s\n",
+		   mode, ndigits, s, sign, decpt,
+		   echo_gcvt(ndigits, '.', 'e', &buf[0], s, sign, decpt));
 	freedtoa(s);
 }
 
 void echo_dvals(const char *title, double dd)
 {
-	printf("\n m |    | %-55s | sign | decpt\n", title);
+	printf("\n m |    | %-55s | sign | decpt | gcvt\n", title);
 	echo_dval(dd, 0, 0);
 	echo_dval(dd, 2, 17);
 	echo_dval(dd, 2, 50);
